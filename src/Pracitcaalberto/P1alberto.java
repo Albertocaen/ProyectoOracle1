b2c3b2d033f2fa1dc14795b5b2526ce84253b1de
Alberto <73608592+Albertocaen@users.noreply.github.com>
2022-09-30 01:00:36 +0200

    Proyecto

---
+++ src/Pracitcaalberto/P1alberto.java
@@ -0,0 +1,10 @@
+package Pracitcaalberto;
+
+public class P1alberto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola mundo:d");
+	}
+
+}
